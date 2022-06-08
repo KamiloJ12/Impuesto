105c888b8cfe729b6c4661d925d06d9d2daab46c
KamiloJ12 <camilo12052000@hotmail.com>
2022-06-08 15:34:21 -0500

    update login

--- impuestos-admin/src/app/components/login/login.component.ts
+++ impuestos-admin/src/app/components/login/login.component.ts
@@ -28,11 +28,7 @@ export class LoginComponent implements OnInit {
 
   login(loginForm: NgForm) {
     if (loginForm.valid) {
-      let data = {
-        email: this.usuario.email,
-        password: this.usuario.password
-      }
-      this._usuarioService.login(data).subscribe(
+      this._usuarioService.login(this.usuario).subscribe(
         response => {
           if(response.user.Rol == "Superadministrador" || response.user.Rol == "Administrador"){
             localStorage.setItem('token', response.tokenReturn);
